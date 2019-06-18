339da789d0c5267f0d5ee0d7087a2f99c3ab23eb
Colin Burr <colin.a.burr@gmail.com>
2019-06-18 21:28:21 +0900

    Add first 2 commands

---
+++ dist/commands.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.initialize = initialize;
+exports.build = build;
+
+function initialize() {}
+
+function build() {}
---
+++ src/commands.js
@@ -0,0 +1,7 @@
+export function initialize() {
+
+}
+
+export function build() {
+
+}
